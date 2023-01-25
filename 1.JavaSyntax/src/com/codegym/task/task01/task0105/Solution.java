6f39178a47955b6228ca162e5cffc294b2119b92
Gishe Tuke <k0365801@kirkwood.edu>
2023-01-24 19:43:41 -0600

    learning variables

---
+++ 1.JavaSyntax/src/com/codegym/task/task01/task0105/Solution.java
@@ -0,0 +1,17 @@
+package com.codegym.task.task01.task0105;
+
+/* 
+Declare variables
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //write your code here
+
+        String name;
+        int age;
+        String city;
+
+    }
+}
