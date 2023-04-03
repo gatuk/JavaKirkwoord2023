ca314b80c7d5cddd5091cac169e4094cb722c301
gatuk <97654660+gatuk@users.noreply.github.com>
2023-04-02 22:48:49 -0500

    Completed CodeGym Level 7

---
+++ 1.JavaSyntax/src/en/codegym/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,37 @@
+package en.codegym.task.pro.task06.task0606;
+/*
+A method for all seasons
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void universalMethod() {
+    }
+    //write your code here
+    public static void universalMethod(int a) {
+    }
+    public static void universalMethod(int a, int b) {
+    }
+    public static void universalMethod(int a, int b, int c) {
+    }
+    public static void universalMethod(int a, int b, int c, int d) {
+
+    }
+    public static void universalMethod(int a, int b, int c, int d, int e) {
+
+    }
+    public static void universalMethod(int a, int b, int c, int d, int e, int f) {
+
+    }
+    public static void universalMethod(int a, int b, int c, int d, int e,int f, int g) {
+
+    }
+    public static void universalMethod(int a, int b, int c, int d, int e,int f, int g, int h) {
+
+    }
+    public static void universalMethod(int a, int b, int c, int d, int e,int f, int g, int h, int i) {
+
+    }
+}
